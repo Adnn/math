97dda9d5bfb102700e43a948d27abb39fd1c28a1
Adrien <Adnn@users.noreply.github.com>
2021-08-02 15:13:35 +0200

    Implement Color ctor to make it easier with bytes, define hdr. Add more predefined colors.

--- src/libs/math/math/Color.h
+++ src/libs/math/math/Color.h
@@ -22,11 +22,19 @@ class Rgb_base : public BASE
 {
     /// \todo Disable most of the available functions
     typedef BASE base_type;
-    using base_type::base_type;
+    // No constructor inheritance (see note on explicit constructor).
 
 public:
     template<class T>
     using derived_type = Rgb_base<T>;
+
+    /// \brief Constructor taking each channel value independently.
+    /// \note This constructor is explicitly defined instead of relying
+    /// on usual constructor inheritance, because otherwise the constexprness
+    /// of literals is lost, causing "narrowing" errors on byte based colors instantiations.
+    constexpr Rgb_base(T_number aRed, T_number aGreen, T_number aBlue) :
+        base_type{aRed, aGreen, aBlue}
+    {}
 
     ACCESSOR_DIMENSION(r, 1)
     ACCESSOR_DIMENSION(g, 2)
@@ -41,15 +49,43 @@ namespace sdr {
     using Rgb = ::ad::math::Rgb_base<std::uint8_t>;
 
 
-    constexpr const Rgb gBlack{(std::uint8_t)0,   (std::uint8_t)0,   (std::uint8_t)0};
-    constexpr const Rgb gWhite{(std::uint8_t)255, (std::uint8_t)255, (std::uint8_t)255};
+    constexpr const Rgb gBlack{0,   0,   0};
+    constexpr const Rgb gWhite{255, 255, 255};
 
-    constexpr const Rgb gRed  {(std::uint8_t)255, (std::uint8_t)0,   (std::uint8_t)0};
-    constexpr const Rgb gGreen{(std::uint8_t)0,   (std::uint8_t)255, (std::uint8_t)0};
-    constexpr const Rgb gBlue {(std::uint8_t)0,   (std::uint8_t)0,   (std::uint8_t)255};
+    constexpr const Rgb gRed  {255, 0,   0};
+    constexpr const Rgb gGreen{0,   255, 0};
+    constexpr const Rgb gBlue {0,   0,   255};
+
+    constexpr const Rgb gYellow {gRed   + gGreen};
+    constexpr const Rgb gCyan   {gGreen + gBlue};
+    constexpr const Rgb gMagenta{gBlue  + gRed};
 
 
     using Grayscale = std::uint8_t;
+
+
+} // namespace sdr
+
+
+namespace hdr {
+
+
+    using Rgb = ::ad::math::Rgb_base<double>;
+
+
+    constexpr const Rgb gBlack{0., 0., 0.};
+    constexpr const Rgb gWhite{1., 1., 1.};
+
+    constexpr const Rgb gRed  {1., 0., 0.};
+    constexpr const Rgb gGreen{0., 1., 0.};
+    constexpr const Rgb gBlue {0., 0., 1.};
+
+    constexpr const Rgb gYellow {gRed   + gGreen};
+    constexpr const Rgb gCyan   {gGreen + gBlue};
+    constexpr const Rgb gMagenta{gBlue  + gRed};
+
+
+    sdr::Rgb to_sdr(Rgb aHdr);
 
 
 } // namespace sdr
@@ -63,6 +99,16 @@ inline std::ostream & operator<<(std::ostream & os, const sdr::Rgb &aRgb)
         << std::setw(3) << static_cast<int>(aRgb.r()) << "; "
         << std::setw(3) << static_cast<int>(aRgb.g()) << "; "
         << std::setw(3) << static_cast<int>(aRgb.b()) << "}";
+}
+
+
+inline sdr::Rgb to_sdr(hdr::Rgb aHdr)
+{
+    return {
+        static_cast<sdr::Rgb::value_type>(std::min(1., aHdr.r()) * 255),
+        static_cast<sdr::Rgb::value_type>(std::min(1., aHdr.g()) * 255),
+        static_cast<sdr::Rgb::value_type>(std::min(1., aHdr.b()) * 255),
+    };
 }
 
 
