be21b787d36faa4d04ac61b37d002a6f178f6b29
Adrien <Adnn@users.noreply.github.com>
2021-08-31 15:03:59 +0200

    Implement interpolation primitives (mostly taken from shmurp).

---
+++ src/apps/tests/tests/Interpolation_tests.cpp
@@ -0,0 +1,167 @@
+#include "catch.hpp"
+
+#include <math/Interpolation.h>
+#include <math/Vector.h>
+
+
+using namespace ad::math;
+
+
+SCENARIO("Clamped type")
+{
+    GIVEN("A few Clamped doubles.")
+    {
+        Clamped<> a{-5};
+        Clamped<> b{0.5};
+        Clamped<> c{1.5};
+
+        THEN("They are clamped as expected")
+        {
+            REQUIRE(a == 0.);
+            REQUIRE(b == 0.5);
+            REQUIRE(c == 1.);
+        }
+    }
+
+    GIVEN("A few Clamped integers")
+    {
+        using Clamp_t = Clamped<int, 3, 10>;
+        Clamp_t a{-5};
+        Clamp_t b{4};
+        Clamp_t c{11};
+
+        THEN("They are clamped as expected")
+        {
+            REQUIRE(a == 3);
+            REQUIRE(b == 4);
+            REQUIRE(c == 10);
+        }
+    }
+}
+
+
+SCENARIO("Linear interpolation")
+{
+    GIVEN("Two vectors")
+    {
+        Vec<2> a{10., 10.};
+        Vec<2> b{20., 40.};
+
+        THEN("They can lerp")
+        {
+            REQUIRE(lerp(a, b,  0.) == a);
+            REQUIRE(lerp(a, b,  1.) == b);
+            REQUIRE(lerp(a, b,  3.) == b);
+            REQUIRE(lerp(a, b, -1.) == a);
+
+            REQUIRE(lerp(a, b,  0.5) == Vec<2>{15., 25.});
+        }
+
+
+        THEN("They can unbound (extrapolations")
+        {
+            REQUIRE(lerpUnbound(a, b,  0.) == a);
+            REQUIRE(lerpUnbound(a, b,  1.) == b);
+            REQUIRE(lerpUnbound(a, b,  3.) == Vec<2>{40., 100.});
+            REQUIRE(lerpUnbound(a, b, -1.) == Vec<2>{ 0., -20.});
+
+            REQUIRE(lerpUnbound(a, b,  0.5) == Vec<2>{15., 25.});
+        }
+    }
+}
+
+
+SCENARIO("ParameterAnimation class")
+{
+    GIVEN("A linear ParameterAnimation and a duration")
+    {
+        float duration = 100.;
+        ParameterAnimation<float> animation{duration};
+
+        THEN("The values are linearly animated")
+        {
+            CHECK(animation.at(0.) == 0.);
+            CHECK(animation.at(50.) == 0.5);
+            CHECK(animation.at(100.) == 1);
+
+            CHECK(animation.at(-10.) == 0.);
+            CHECK(animation.at(1000.) == 1.);
+        }
+
+        THEN("The animation can be advanced, the advance is cumulative")
+        {
+            CHECK(animation.advance(0.) == 0.);
+            CHECK(animation.advance(10.) == Approx(0.1));
+            CHECK(animation.advance(10.) == Approx(0.2));
+            CHECK(animation.advance(10.) == Approx(0.3));
+
+            CHECK_FALSE(animation.isCompleted());
+
+            CHECK(animation.advance(70.) == 1.);
+            CHECK(animation.isCompleted());
+
+            CHECK(animation.advance(70.) == 1.);
+            CHECK(animation.isCompleted());
+
+            THEN("Random values can still be queried")
+            {
+                CHECK(animation.at(0.) == 0.);
+                CHECK(animation.at(50.) == 0.5);
+                CHECK(animation.at(100.) == 1);
+
+                CHECK(animation.at(-10.) == 0.);
+                CHECK(animation.at(1000.) == 1.);
+            }
+        }
+    }
+}
+
+
+SCENARIO("Interpolation class")
+{
+    GIVEN("A linear and a smoothstep Interpolations betwen two vectors")
+    {
+        Vec<2> a{10., 10.};
+        Vec<2> b{20., 40.};
+
+        double duration = 10;
+        auto linear = makeInterpolation<ease::Linear>(a, b, duration);
+        auto smoothstep = makeInterpolation<ease::SmoothStep>(a, b, duration);
+
+        WHEN("The interpolations advances")
+        {
+            Vec<2> interpolatedHalf = linear.advance(duration/2.);
+            // Note that this advance will be on top of previous advance
+            Vec<2> interpolatedThreeQuarter = linear.advance(duration/4.);
+
+            Vec<2> smoothHalf = smoothstep.advance(duration/2.);
+            // Note that this advance will be on top of previous advance
+            Vec<2> smoothThreeQuarter = smoothstep.advance(duration/4.);
+
+            REQUIRE_FALSE(linear.isCompleted());
+            REQUIRE_FALSE(smoothstep.isCompleted());
+
+            THEN("The result is interpolated")
+            {
+                REQUIRE(interpolatedHalf         == a + 1./2. * (b - a));
+                REQUIRE(interpolatedThreeQuarter == a + 3./4. * (b - a));
+
+                REQUIRE(interpolatedHalf         == smoothHalf);
+                REQUIRE(interpolatedThreeQuarter.x() <  smoothThreeQuarter.x()); 
+                REQUIRE(interpolatedThreeQuarter.y() <  smoothThreeQuarter.y()); 
+            }
+
+            WHEN("They are advanced to or past the end.")
+            {
+                linear.advance(duration/4.);
+                smoothstep.advance(duration);
+
+                THEN("They indicate completion")
+                {
+                    REQUIRE(linear.isCompleted());
+                    REQUIRE(smoothstep.isCompleted());
+                }
+            }
+        }
+    }
+}
---
+++ src/libs/math/math/Interpolation.h
@@ -0,0 +1,189 @@
+#pragma once
+
+
+#include <type_traits>
+
+
+namespace ad {
+namespace math {
+
+
+/// \brief Clamps a value given on construction to the static interval [N_low, N_high].
+template <class T_value = double,
+          int N_low = 0,
+          int N_high = 1
+          // Requires C++20
+          //auto F_low = []{ return (T_value)0; },
+          //auto F_high = []{ return (T_value)1; }
+          >
+class Clamped
+{
+public:
+    static constexpr T_value low_v = N_low;
+    static constexpr T_value high_v = N_high;
+
+    /*implicit*/ constexpr Clamped(const T_value & aValue);
+
+    const T_value & value() const
+    { return mValue; }
+
+    /*implicit*/ operator const T_value & () const
+    { return value(); }
+
+private:
+    T_value mValue;
+};
+
+
+template <class T_value, int N_low, int N_high>
+constexpr Clamped<T_value, N_low, N_high>::Clamped(const T_value & aValue) :
+    mValue{std::clamp(aValue, low_v, high_v)}
+{}
+
+
+/// \brief Bound linear interpolation between aFirst and aLast.
+template <class T_value, class T_parameter>
+T_value lerp(const T_value & aFirst, const T_value & aLast, const Clamped<T_parameter> & aParameter)
+{
+    return aFirst + aParameter * (aLast - aFirst);
+}
+
+
+/// \brief Bound linear interpolation between aFirst and aLast.
+template <class T_value, class T_parameter>
+T_value lerp(const T_value & aFirst, const T_value & aLast, const T_parameter & aParameter)
+{
+    return aFirst + Clamped<T_parameter>{aParameter} * (aLast - aFirst);
+}
+
+
+/// \brief Unbound linear interpolation/extrapolation about aFirst and aLast.
+template <class T_value, class T_parameter>
+T_value lerpUnbound(const T_value & aFirst, const T_value & aLast, const T_parameter & aParameter)
+{
+    return aFirst + aParameter * (aLast - aFirst);
+}
+
+
+/// \brief Namespace containing all the easing functions.
+namespace ease
+{
+    template <class T_parameter>
+    struct Linear
+    {
+        static T_parameter ease(T_parameter aInput)
+        { return aInput; }
+    } ;
+
+    /// \see https://en.wikipedia.org/wiki/Smoothstep
+    template <class T_parameter>
+    struct SmoothStep
+    {
+        static T_parameter ease(T_parameter aInput)
+        {
+            Clamped<T_parameter> x{aInput}; 
+            return std::pow(x, 2) * (3 - 2 * x);
+        }
+    } ;
+}
+
+
+
+/// \brief Applies a static easing function to an input parameter, over a time duration.
+///
+/// It allows to apply an easing functions (quadratic, etc.) to a parameter, before
+/// feeding it to a linerar interpolation function.
+template <class T_parameter, class T_easeFunctor = ease::Linear<T_parameter>>
+class ParameterAnimation
+{
+public:
+    using Result_type = Clamped<T_parameter>;
+
+    
+    ParameterAnimation(T_parameter aDuration) :
+        mSpeedFactor{(T_parameter)1 / aDuration}
+    {}
+
+
+    Result_type at(T_parameter aInput)
+    {
+        return T_easeFunctor::ease(mSpeedFactor * aInput);
+    }
+
+
+    Result_type advance(T_parameter aIncrement)
+    {
+        return at(mAccumulatedInput += aIncrement);
+    }
+
+
+    bool isCompleted() const
+    {
+        return mAccumulatedInput * mSpeedFactor >= Result_type::high_v;
+    }
+
+
+private:
+    T_parameter mSpeedFactor;
+    T_parameter mAccumulatedInput{Result_type::low_v};
+};
+
+
+/// \brief Helper factory for ParameterAnimation.
+template <template <class> class TT_easeFunctor, class T_parameter>
+ParameterAnimation<T_parameter, TT_easeFunctor<T_parameter>> makeParameterAnimation(T_parameter aDuration)
+{
+    return {aDuration} ;
+}
+
+
+/// \brief Encapsulate a complete interpolation procedure, with bounds,
+/// duration, and easing.
+template <class T_value, class T_parameter, class T_easeFunctor>
+class Interpolation
+{
+public:
+    using Animation_type = ParameterAnimation<T_parameter, T_easeFunctor>;
+
+    Interpolation(T_value aFirst, T_value aLast, Animation_type aAnimation) :
+        mFirst{std::move(aFirst)},
+        mLast{std::move(aLast)},
+        mAnimation{std::move(aAnimation)}
+    {}
+
+
+    T_value advance(T_parameter aIncrement)
+    {
+        return lerp(mFirst, mLast, mAnimation.advance(aIncrement));
+    }
+
+
+    bool isCompleted() const
+    {
+        return mAnimation.isCompleted();
+    }
+
+
+private:
+    T_value mFirst;
+    T_value mLast;
+    Animation_type mAnimation;
+};
+
+
+/// \brief Helper factory for Interpolation.
+template <template <class> class TT_easeFunctor, class T_value, class T_parameter>
+Interpolation<T_value, T_parameter, TT_easeFunctor<T_parameter>>
+makeInterpolation(const T_value & aFirst, const T_value & aLast, T_parameter aDuration)
+{
+    static_assert(! std::is_integral_v<T_parameter>,
+                  "Using an integral parameter to interpolate between [0..1] is very likely an error.");
+    return Interpolation(
+        aFirst,
+        aLast,
+        makeParameterAnimation<TT_easeFunctor>(aDuration));
+}
+
+
+} // namespace math
+} // namespace ad
