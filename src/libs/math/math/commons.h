4ff23b220ef13169cff0b36c7dacdf0a2a2f9260
Francois Poizat <francois.poizat@gmail.com>
2023-10-05 13:45:45 +0200

    adds a espilon comparison for float

--- src/libs/math/math/commons.h
+++ src/libs/math/math/commons.h
@@ -1,12 +1,18 @@
 #pragma once
 
 
+#include <cmath>
 namespace ad {
 namespace math {
 
 
 typedef double real_number;
 
+template<class T_number>
+bool epsilonCompare(T_number a, T_number b)
+{
+    return std::fabs(a - b ) < std::numeric_limits<T_number>::epsilon();
+}
 
 }} // namespace ad::math
 
